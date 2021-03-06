8a993f465b8c263f38fdd3d9bdcb1f747b97c48f
kbbushman <k.bushman@yahoo.com>
2021-03-06 00:33:08 -0800

    Adds custom styles to card

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -17,6 +17,16 @@ const useStyles = makeStyles((theme) => ({
   songInfoContainer: {
     display: 'flex',
     alignItems: 'center',
+  },
+  songInfo: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  thumbnail: {
+    objectFit: 'cover',
+    width: 140,
+    height: 140,
   }
 }));
 
@@ -52,27 +62,30 @@ function SongList() {
 }
 
 function Song({ song }) {
+  const classes = useStyles();
   const { title, artist, thumbnail } = song;
   return (
-    <Card>
-      <div>
-        <CardMedia image={thumbnail} />
-        <CardContent>
-          <Typography gutterBottom variant='h5' component='h2'>
-            {title}
-          </Typography>
-          <Typography variant='body1' component='p' color='textSecondary'>
-            {artist}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton size='small' color='primary'>
-            <PlayArrow />
-          </IconButton>
-          <IconButton size='small' color='secondary'>
-            <Save />
-          </IconButton>
-        </CardActions>
+    <Card className={classes.container}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.thumbnail} />
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+              {title}
+            </Typography>
+            <Typography variant='body1' component='p' color='textSecondary'>
+              {artist}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size='small' color='primary'>
+              <PlayArrow />
+            </IconButton>
+            <IconButton size='small' color='secondary'>
+              <Save />
+            </IconButton>
+          </CardActions>
+        </div>
       </div>
     </Card>
   );
