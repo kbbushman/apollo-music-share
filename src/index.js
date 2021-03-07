60528780aac9d560c970f076e5e47fac1767c5d7
kbbushman <k.bushman@yahoo.com>
2021-03-06 19:50:26 -0800

    Fixes missing ApolloProvider client prop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
 import App from './App';
 import theme from './theme';
 import client from './graphql/client';
 
 ReactDOM.render(
-  <React.StrictMode client={client}>
-    <ApolloProvider>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <App />
