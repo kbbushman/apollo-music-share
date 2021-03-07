ab24bb625a4fbde08d35715aec5a7c90d128aebf
kbbushman <k.bushman@yahoo.com>
2021-03-06 16:51:12 -0800

    Adds media query for song player grid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Header from './components/Header';
 import AddSong from './components/AddSong';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
-import { Grid } from '@material-ui/core';
+import { Grid, useMediaQuery } from '@material-ui/core';
 
 function App() {
+  const greaterThanMd = useMediaQuery((theme) => theme.breakpoints.up('md'));
+
   return (
     <>
       <Header />
@@ -13,11 +15,17 @@ function App() {
           <AddSong />
           <SongList />
         </Grid>
-        <Grid style={{
+        <Grid style={
+          greaterThanMd ? {
           position: 'fixed',
           width: '100%',
           right: 0,
           top: 70,
+        } : {
+          position: 'fixed',
+          width: '100%',
+          left: 0,
+          bottom: 0.
         }} xs={12} md={5} item>
           <SongPlayer />
         </Grid>
