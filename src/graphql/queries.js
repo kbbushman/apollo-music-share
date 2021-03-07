52f0dc548d2956bb90a9a72e47a81c30bbceddc1
kbbushman <k.bushman@yahoo.com>
2021-03-06 19:50:49 -0800

    Adds duration to getSongs query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -4,11 +4,11 @@ export const GET_SONGS = gql`
   query getSongs {
     songs(order_by: {created_at: desc}) {
       artist
-      duration
       id
       title
       thumbnail
       url
+      duration
     }
   }
 `
