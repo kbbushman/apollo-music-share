5efe8999e22812b7ba0159de965d800ece46015f
kbbushman <k.bushman@yahoo.com>
2021-03-06 18:21:55 -0800

    Adds ApolloProvider with client

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
 import theme from './theme';
+import client from './graphql/client';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </MuiThemeProvider>
+  <React.StrictMode client={client}>
+    <ApolloProvider>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
