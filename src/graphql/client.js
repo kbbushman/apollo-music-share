d782dbea9bc4e6a5b1814fe48a93decbd767dd66
kbbushman <k.bushman@yahoo.com>
2021-03-07 22:39:01 -0800

    Updates graphql endpoint

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'https://apollo-music-api-v2.hasura.app/v1/graphql',
+  uri: 'https://apollo-music-database.herokuapp.com/v1/graphql',
 });
 
 export default client;
