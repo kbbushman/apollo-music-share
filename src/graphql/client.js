65f742fe2089e618f99dda4c77adee77a4e11582
kbbushman <k.bushman@yahoo.com>
2021-03-07 23:07:18 -0800

    Refactors client for graphql ws subscriptions

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,20 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'https://apollo-music-database.herokuapp.com/v1/graphql',
+  link: new WebSocketLink({
+    uri: 'wss://apollo-music-database.herokuapp.com/v1/graphql',
+    options: {
+      reconnect: true,
+    }
+  }),
+  cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   uri: 'https://apollo-music-database.herokuapp.com/v1/graphql',
+// });
 
 export default client;
