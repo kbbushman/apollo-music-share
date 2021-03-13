36929472525af2c2e7924d12acefbf68571b5d5d
kbbushman <k.bushman@yahoo.com>
2021-03-13 15:29:25 -0800

    Adds song type defs for local storage song queue management

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -12,7 +12,42 @@ const client = new ApolloClient({
     }
   }),
   cache: new InMemoryCache(),
+  typeDefs: gql`
+    type Song {
+      id: ID!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      url: String!
+      duration: Float!
+    }
+
+    input SongInput {
+      id: ID!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      url: String!
+      duration: Float!
+    }
+
+    type Query {
+      queue: [Song]!
+    }
+
+    type Mutation {
+      addOrRemoveFromQueue(input: SongInput): [Song]!
+    }
+  `,
 });
+
+const data = {
+  queue: []
+};
+
+// For Managing Song Queue in LocalStorage
+// Uses typeDefs set on client
+client.writeData(data);
 
 // const client = new ApolloClient({
 //   uri: 'https://apollo-music-share-api.herokuapp.com/graphql',
