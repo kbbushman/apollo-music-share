921cc0335dc8f2897addfcc332f7ab8562aee40d
kbbushman <k.bushman@yahoo.com>
2021-03-13 15:59:48 -0800

    Adds addOrRemoveFromQueue client mutation resolver

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,6 +2,7 @@ import ApolloClient from 'apollo-client';
 import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { gql } from '@apollo/client';
+import { GET_QUEUED_SONGS } from './queries';
 // import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
@@ -40,6 +41,32 @@ const client = new ApolloClient({
       addOrRemoveFromQueue(input: SongInput): [Song]!
     }
   `,
+  resolvers: {
+    Mutation: {
+      addOrRemoveFromQueue: (_, { input }, { cache }) => {
+        const queryResult = cache.readQuery({
+          query: GET_QUEUED_SONGS
+        });
+
+        if (queryResult) {
+          const { queue } = queryResult;
+          const isInQueue = queue.some((song) => song.id === input.id);
+          const newQueue = isInQueue ?
+            queue.filter((song) => song.id !== input.id)
+            : [...queue, input];
+
+          cache.writeQuery({
+            query: GET_QUEUED_SONGS,
+            data: { queue: newQueue },
+          });
+
+          return newQueue;
+        }
+        
+        return [];
+      },
+    },
+  },
 });
 
 const data = {
