b1cb4052179cd70c19974c635c8d8ccb597b84dc
kbbushman <k.bushman@yahoo.com>
2021-03-13 14:39:43 -0800

    Adds play song case

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,10 @@
 function songReducer(state, action) {
   switch (action.type) {
+    case 'PLAY_SONG':
+      return {
+        ...state,
+        isPlaying: true,
+      };
     default:
       return state;
   }
